2456c222a04cd26129d5d5693e3813c397bdb499
ddd-dong <dong1214.mailbox@gmail.com>
2022-08-14 00:31:25 +0800

    s

---
+++ assets/js/fromddd.js
@@ -0,0 +1,8 @@
+console.log("我本人林育廷只負責把網站放上去，不保證其內容之正確性、公正性並不負相關責任")
+console.log("關於林育廷之相關紀載不保證其可靠性")
+console.log("有問題找日凱(propanal93@gmail.com)")
+console.log("ddd()")
+function ddd(){
+    document.getElementById("wrapper").innerHTML=`<iframe width="1519" height="545" src="https://www.youtube.com/embed/8KU5Bp0QSE4" title="火鍋端統神" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+// 雖然應該沒有人會翻到這，不過這免強是彩蛋吧? WAH!
